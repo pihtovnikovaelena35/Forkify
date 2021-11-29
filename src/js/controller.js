bba7f38b612f8eaff42dc3f2ff38a74678004088
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2021-11-29 11:44:46 +0100

    Implementing search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,31 +1,25 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
-
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+  
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
 
-const recipeContainer = document.querySelector('.recipe');
+if (module.hot) {
+  module.hot.accept();
+}
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
 
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
-    recipeView.renderSprinner();
-
+    recipeView.renderSpinner();
+ 
     // 1.Loading recipe 
     await model.loadRecipe(id);
 
@@ -36,7 +30,32 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();                                                 
+
+    // 1. Get search query 
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //2. Load search results
+    await model.loadSearchResults(query);
+
+    //3. Render results
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
+
+
+
+
+// https://forkify-api.herokuapp.com/v2
