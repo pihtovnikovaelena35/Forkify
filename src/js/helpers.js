255a84f56c6a2f6f962c7ee6581b7bab300f6c87
Elena Pihtovnikova <pihtovnikovaelena35@gmail.com>
2021-12-24 15:44:35 +0100

    comment out timeout

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -4,7 +4,7 @@ import { TIMEOUT_SEC } from './config.js';
 const timeout = function (s) {
   return new Promise(function (_, reject) {
     setTimeout(function () {
-       reject(new Error(`Request took too long! Timeout after ${s} second`));
+       //reject(new Error(`Request took too long! Timeout after ${s} second`));
     }, s * 1000);
   });
 };
